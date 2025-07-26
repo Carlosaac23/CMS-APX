e591f484c6e823fc8a2a165d48ce60daa8f13bd1
Carlos <carlosaac232001@gmail.com>
2025-07-26 00:15:28 -0500

    add main logic to main.js

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-function addWorkCard() {
+function addWorkCard(params = {}) {
   const template = document.querySelector('#portfolio__card-template');
   const container = document.querySelector('.portfolio__content');
+
+  template.content.querySelector('.portfolio__card-title').textContent = params.title;
+  template.content.querySelector('.portfolio__card-text').textContent = params.description;
+  template.content.querySelector('.portfolio__img').src = params.image;
+  template.content.querySelector('.portfolio__card-link').href = params.url;
 
   const clone = document.importNode(template.content, true);
   container.appendChild(clone);
 }
 
+function getContentfulData() {}
+
 function main() {
-  addWorkCard();
-  addWorkCard();
-  addWorkCard();
+  const contentfulData = getContentfulData();
 }
 
 main();
